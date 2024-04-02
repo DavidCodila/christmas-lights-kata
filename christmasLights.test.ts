5ea5f1e5080429ceb0adc5c37401bb54c4d891b2
David <david@Davids-MacBook-Pro.local>
2024-04-02 13:51:33 +1100

    Created test for second scenario

--- christmasLights.test.ts
+++ christmasLights.test.ts
@@ -3,7 +3,13 @@ import { turnOnLights } from "./christmasLights";
 import * as christmasLightsExpected from "./christmasLightsScenariosExpected";
 
 test("should turn on 8,8 through 9,10", () => {
-  const scenaro_1 = [8, 8, 9, 10];
-  const result = turnOnLights(scenaro_1);
+  const scenario_1 = [7, 7, 8, 9];
+  const result = turnOnLights(scenario_1);
   expect(result).toEqual(christmasLightsExpected.scenario1);
+});
+
+test("should turn on 4,5 through 8,4", () => {
+  const scenario_2 = [3, 2, 7, 3];
+  const result = turnOnLights(scenario_2);
+  expect(result).toEqual(christmasLightsExpected.scenario2);
 });
