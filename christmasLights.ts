e804c04b6d3a0043a87beb469c5080004c17ba03
David <david@Davids-MacBook-Pro.local>
2024-04-02 14:32:19 +1100

    Altered main function to not be hard coded and pass all tests

--- christmasLights.ts
+++ christmasLights.ts
@@ -3,30 +3,19 @@ import { createLightsArray } from "./createLightsArray";
 export function turnOnLights(
   rectangleCornersCoordinates: number[]
 ): string[][] {
-  if (rectangleCornersCoordinates[0] == 3) {
-    return [
-      ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-      ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-      ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-      ["0", "0", "1", "1", "0", "0", "0", "0", "0", "0"],
-      ["0", "0", "1", "1", "0", "0", "0", "0", "0", "0"],
-      ["0", "0", "1", "1", "0", "0", "0", "0", "0", "0"],
-      ["0", "0", "1", "1", "0", "0", "0", "0", "0", "0"],
-      ["0", "0", "1", "1", "0", "0", "0", "0", "0", "0"],
-      ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-      ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ];
-  }
-  return [
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "1", "1", "1"],
-    ["0", "0", "0", "0", "0", "0", "0", "1", "1", "1"],
-    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+  var blankLightsArray = createLightsArray();
+  let topLeftCorner = [
+    rectangleCornersCoordinates[0],
+    rectangleCornersCoordinates[1],
   ];
+  let bottomRightCorner = [
+    rectangleCornersCoordinates[2],
+    rectangleCornersCoordinates[3],
+  ];
+  for (var i = topLeftCorner[0]; i <= bottomRightCorner[0]; i++) {
+    for (var j = topLeftCorner[1]; j <= bottomRightCorner[1]; j++) {
+      blankLightsArray[i][j] = "1";
+    }
+  }
+  return blankLightsArray;
 }
