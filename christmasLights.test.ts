7f1a43e74f77654aceb9b4d49c02f2a867d0ff7e
David <david@Davids-MacBook-Pro.local>
2024-04-02 14:06:09 +1100

    Added third unit test

--- christmasLights.test.ts
+++ christmasLights.test.ts
@@ -2,14 +2,20 @@ import { test, expect } from "bun:test";
 import { turnOnLights } from "./christmasLights";
 import * as christmasLightsExpected from "./christmasLightsScenariosExpected";
 
-test("should turn on 8,8 through 9,10", () => {
+test("should turn on 7,7 through 8,9", () => {
   const scenario_1 = [7, 7, 8, 9];
   const result = turnOnLights(scenario_1);
   expect(result).toEqual(christmasLightsExpected.scenario1);
 });
 
-test("should turn on 4,5 through 8,4", () => {
+test("should turn on 3,2 through 7,3", () => {
   const scenario_2 = [3, 2, 7, 3];
   const result = turnOnLights(scenario_2);
   expect(result).toEqual(christmasLightsExpected.scenario2);
+});
+
+test("should turn on 4,8 through 7,4", () => {
+  const scenario_3 = [3, 5, 5, 8];
+  const result = turnOnLights(scenario_3);
+  expect(result).toEqual(christmasLightsExpected.scenario3);
 });
